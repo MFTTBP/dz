7c512f2d1da7ef8731f74bb827fbae049c9fdc1f
MFTTBP <rebro_leorika@bk.ru>
2025-11-30 14:53:05 +0300

    my dz

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-#my dz
+DRINKS = "напиток → «чай», «кофе», «сок»"
+SOUP = "суп → «борщ», «щи», «суп-пюре»"
+DESSERT = "десерт → «торт», «мороженое», «фрукты»"
+
+print(DRINKS)
+print(SOUP)
+print(DESSERT)
+
+menu = input("")
+
+if menu == "напиток":
+    print(DRINKS)
+elif menu == "суп":
+    print(SOUP)
+elif menu == "десерт":
+    print(DESSERT)
+else:
+    print("Выберите категорию из списка!")
+
+price = input("")
+
+match price:
+    case "чай":
+        print("40p")
+    case "кофе":
+        print("70р")
+    case "сок":
+        print("100р")
+    case "борщ":
+        print("120р")
+    case "щи":
+        print("110р")
+    case "суп-пюре":
+        print("140р")
+    case "торт":
+        print("100р")
+    case "мороженное":
+        print("80р")
+    case "фрукты":
+        print("130р")
+    case _:
+        print("ошибка")
