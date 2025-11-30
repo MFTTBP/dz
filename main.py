25674115fba1490e544fe3c59c68ac3718a1acc4
MFTTBP <rebro_leorika@bk.ru>
2025-11-30 14:58:47 +0300

    ispravil_2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ print(DRINKS)
 print(SOUP)
 print(DESSERT)
 
-menu = input("")
+menu = input("Выберите категорию: ")
 
 if menu == "напиток":
     print(DRINKS)
@@ -17,7 +17,7 @@ elif menu == "десерт":
 else:
     print("Выберите категорию из списка!")
 
-price = input("")
+price = input("Выберите блюдо: ")
 
 match price:
     case "чай":
